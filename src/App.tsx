5d61d1de493a5c969dba6248951c4f2d38d65965
Andrei Puchko <andrei.puchko@gmx.de>
2025-02-13 20:21:14 +0100

    Close dialog button

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
-import GridData from "./data_modules/data"
-import { columns } from "./data_modules/data"
+import GridData from "./data_modules/data";
+import { columns } from "./data_modules/data";
 
-import DridComponent from './components/DataGrid'
-import Dialog from './components/Dialog'
-import MainMenu from './components/MainMenu'
+import DridComponent from './components/DataGrid';
+import Dialog from './components/Dialog';
+import MainMenu from './components/MainMenu';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [showDialog, setShowDialog] = useState(false);
 
   return (
     <>
-      <MainMenu />
-      <Dialog>
-        <DridComponent columns={columns} data={GridData} />
-      </Dialog>
+      <MainMenu onShowDataGrid={() => setShowDialog(true)} />
+      {showDialog && (
+        <Dialog onClose={() => setShowDialog(false)}>
+          <DridComponent columns={columns} data={GridData} />
+        </Dialog>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
