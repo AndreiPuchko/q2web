a32cb08339c323f756e3c6cbb9196c3c33f2ea9d
Andrei Puchko <andrei.puchko@gmx.de>
2025-02-14 13:53:41 +0100

    MainMenu from data, desktop look

--- src/components/MainMenu.tsx
+++ src/components/MainMenu.tsx
@@ -1,15 +1,35 @@
 import React from 'react';
 import './MainMenu.css';
+import { forms } from '../data_modules/data';
 
 interface MainMenuProps {
   onShowDataGrid: (formKey: string) => void;
 }
 
 const MainMenu: React.FC<MainMenuProps> = ({ onShowDataGrid }) => {
+  const menuStructure = Object.keys(forms).reduce((acc, key) => {
+    const [mainMenu, subMenu] = forms[key].menubarpath.split('|');
+    if (!acc[mainMenu]) {
+      acc[mainMenu] = [];
+    }
+    acc[mainMenu].push({ key, label: subMenu });
+    return acc;
+  }, {} as Record<string, { key: string, label: string }[]>);
+
   return (
     <nav className='MainMenuBar'>
-      <button onClick={() => onShowDataGrid("datagrid1")}>Show Data Grid 1</button>
-      <button onClick={() => onShowDataGrid("datagrid2")}>Show Data Grid 2</button>
+      {Object.keys(menuStructure).map((mainMenu) => (
+        <div className='dropdown' key={mainMenu}>
+          <button className='dropbtn'>{mainMenu}</button>
+          <div className='dropdown-content'>
+            {menuStructure[mainMenu].map((item) => (
+              <button key={item.key} onClick={() => onShowDataGrid(item.key)}>
+                {item.label}
+              </button>
+            ))}
+          </div>
+        </div>
+      ))}
     </nav>
   );
 };
