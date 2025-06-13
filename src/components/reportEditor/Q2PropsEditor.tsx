e05c79e20e206151ad231507ba2518a0f399d48c
Andrei Puchko <andrei.puchko@gmx.de>
2025-06-13 12:06:30 +0200

    props editor

--- src/components/reportEditor/Q2PropsEditor.tsx
+++ src/components/reportEditor/Q2PropsEditor.tsx
@@ -17,7 +17,6 @@ class Q2PropsEditor extends Component {
     });
 
     if (propEditor.add_control("/f", "Font")) {
-
       propEditor.add_control("font_family", "Font family", { datalen: 15, check: true });
       propEditor.add_control("font_size", "Font size", { datalen: 3, check: true });
       propEditor.add_control("font_weight", "Bold", { control: "check", data: true, check: true });
@@ -25,33 +24,33 @@ class Q2PropsEditor extends Component {
       propEditor.add_control("font_underline", "Unterline", { control: "check", data: true, check: true });
       propEditor.add_control("/s", "");
       propEditor.add_control("/");
-
-      if (propEditor.add_control("/h", "Borders", { alignment: 4, check: true })) {
-        propEditor.add_control("border_left", "", { datalen: 3 });
-        propEditor.add_control("/v", "");
-        propEditor.add_control("border_top", "", { datalen: 3 });
-        propEditor.add_control("border_bottom", "", { datalen: 3 });
-        propEditor.add_control("/");  // close layout
-        propEditor.add_control("border_right", "", { datalen: 3 });
-        propEditor.add_control("/s", "");
-        propEditor.add_control("/");  // close layout
-      }
-      if (propEditor.add_control("/h", "Paddings", { alignment: 4, check: true })) {
-        propEditor.add_control("padding_left", "", { datalen: 5 });
-        propEditor.add_control("/v", "", { datalen: 6 });
-        propEditor.add_control("padding_top", "", { datalen: 5 });
-        propEditor.add_control("padding_bottom", "", { datalen: 5 });
-        propEditor.add_control("/");  // close layout
-        propEditor.add_control("padding_right", "", { datalen: 5 });
-        propEditor.add_control("/s", "");
-        propEditor.add_control("/");  // close layout
-      }
-      if (propEditor.add_control("/v", "Aligments", { alignment: 8 })) {
-        propEditor.add_control("text_align", "Horizontal", { pic: "Left;Center;Right;Justify", control: "radio", data: 1, check: true });
-        propEditor.add_control("vertical_align", "Vertical", { pic: "Top;Middle;Bottom", control: "radio", data: 2, check: true });
-        propEditor.add_control("/");  // close layout
-      }
     }
+    if (propEditor.add_control("/h", "Borders", { alignment: 4, check: true })) {
+      propEditor.add_control("border_left", "", { datalen: 3 });
+      propEditor.add_control("/v", "");
+      propEditor.add_control("border_top", "", { datalen: 3 });
+      propEditor.add_control("border_bottom", "", { datalen: 3 });
+      propEditor.add_control("/");  // close layout
+      propEditor.add_control("border_right", "", { datalen: 3 });
+      propEditor.add_control("/s", "");
+      propEditor.add_control("/");  // close layout
+    }
+    if (propEditor.add_control("/h", "Paddings", { alignment: 4, check: true })) {
+      propEditor.add_control("padding_left", "", { datalen: 5 });
+      propEditor.add_control("/v", "", { datalen: 6 });
+      propEditor.add_control("padding_top", "", { datalen: 5 });
+      propEditor.add_control("padding_bottom", "", { datalen: 5 });
+      propEditor.add_control("/");  // close layout
+      propEditor.add_control("padding_right", "", { datalen: 5 });
+      propEditor.add_control("/s", "");
+      propEditor.add_control("/");  // close layout
+    }
+    if (propEditor.add_control("/v", "Aligments", { alignment: 8 })) {
+      propEditor.add_control("text_align", "Horizontal", { pic: "Left;Center;Right;Justify", control: "radio", data: 1, check: true });
+      propEditor.add_control("vertical_align", "Vertical", { pic: "Top;Middle;Bottom", control: "radio", data: 2, check: true });
+      propEditor.add_control("/");  // close layout
+    }
+
 
     return propEditor
   }
@@ -68,7 +67,7 @@ class Q2PropsEditor extends Component {
           style={{
             background: "",
             color: "black",
-            fontSize: "14px",
+            fontSize: "12px",
             border: "1px solid gray",
           }}
         >
