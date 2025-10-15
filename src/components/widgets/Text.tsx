bab1f423feaf5bef01b8a6a859a4930b1b2e5a09
Andrei Puchko <andrei.puchko@gmx.de>
2025-10-15 22:39:32 +0200

    Q2Text - onChange

--- src/components/widgets/Text.tsx
+++ src/components/widgets/Text.tsx
@@ -13,6 +13,13 @@ export class Q2Text extends Q2Widget<Q2TextProps, Q2TextState> {
         super(props);
     }
 
+    handleChange = (e: React.ChangeEvent<HTMLInputElement> | { target: { value: string, name?: string } }) => {
+        const { column } = this.props;
+        const value = (e as any).target.value;
+        column.data = value;
+        this.setState({ value }, () => { this.changed(value) });
+    };
+    
     render() {
         const { column } = this.props;
         const readOnly = !!column.readonly;
@@ -22,10 +29,11 @@ export class Q2Text extends Q2Widget<Q2TextProps, Q2TextState> {
 
         return (
             <textarea
-                key={this.id}
+                key={column.key}
                 className="Q2Text"
                 value={column.data}
                 readOnly={readOnly}
+                onChange={this.handleChange}
                 style={style}
                 id={this.id}
                 name={column.column}
